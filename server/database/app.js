36f1da5a49a8854111a0a5cf9494a7d2ee829a09
edgar <edgarchwong@gmail.com>
2024-10-09 21:30:35 +0100

    Update

--- server/database/app.js
+++ server/database/app.js
@@ -56,19 +56,35 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
   }
 });
 
-// Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const dealerships = await Dealerships.find();
+    res.json(dealerships);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
-
-// Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const state = req.params.state;
+    const dealerships = await Dealerships.find({ state });
+    res.json(dealerships);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships by state' });
+  }
 });
-
-// Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const dealerId = req.params.id;
+    const dealership = await Dealerships.findOne({ id: dealerId });
+    if (dealership) {
+      res.json(dealership);
+    } else {
+      res.status(404).json({ error: 'Dealership not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealership by ID' });
+  }
 });
 
 //Express route to insert review
